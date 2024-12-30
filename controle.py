da68b20d3bcef4ea2010c86e0440aa4307db056b
AlonMoreira <alissonlim13@gmail.com>
2024-12-30 19:46:27 -0300

    Criação do modelo

--- controle.py
+++ controle.py
@@ -1,6 +1,14 @@
 from dotenv import load_dotenv
 import os
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain.schema import SystemMessage, HumanMessage
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 chave_api = os.getenv('OPIA_API_KEY')
+
+mensagens = [
+    SystemMessage('Traduza o texto a seguir para inglês'), 
+    HumanMessage('Se inscrevam no canal para aprender Python')
+]
+
+modelo = ChatOpenAI(model='gpt-4o-mini')
